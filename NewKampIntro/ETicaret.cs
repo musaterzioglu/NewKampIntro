94c13770e43d6f6b02afda9c57685a5ca9fdb075
musaterzioglu <42242692+musaterzioglu@users.noreply.github.com>
2023-07-02 22:07:41 +0300

    Add project files.

---
+++ NewKampIntro/ETicaret.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewKampIntro
+{
+    class ETicaret
+    {
+      
+        static void Main(string[] args)
+        {
+            string[] computers = new string[] {"LENOVO","CASPER","HP","DELL","ASUS" };
+
+            for (int i = 0; i < computers.Length; i++)
+            {
+                Console.WriteLine(computers[i]);
+            }
+
+            foreach (string computer in computers)
+            {
+                Console.WriteLine(computer);
+            }
+
+        }
+        
+
+    }
+}
---
+++ NewKampIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewKampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 3200000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            
+        }
+    }
+}
